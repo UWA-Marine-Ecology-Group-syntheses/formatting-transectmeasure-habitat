9378abc7de77f7ac91b6ec6478d305fa7918b71e
Abbey Gibbons <abbey.gibbons@uwa.edu.au>
2026-07-02 14:55:29 +0800

    formatting data for GA upload

--- r/2023-03_SwC_stereo-BRUVs.R
+++ r/2023-03_SwC_stereo-BRUVs.R
@@ -16,7 +16,7 @@ metadata <- read_metadata(here::here("data/2023-03_SwC_stereo-BRUVs/")) %>%
   glimpse()
 
 # read in forwards annotations
-forwards <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_random-points_forwards_Dot Point Measurements.txt", 
+forwards <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_Forwards_Dot Point Measurements.txt", 
                        header = T, skip = 4, stringsAsFactors = FALSE, 
                        colClasses = "character", na.strings = "") %>%
   clean_names() %>%
@@ -24,35 +24,42 @@ forwards <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_r
   dplyr::mutate(filename = str_replace_all(filename, "take 2", ""))
 
 # read in forwards annotations
-backwards <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_random-points_backwards_Dot Point Measurements.txt", 
+backwards <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_Backwards_Dot Point Measurements.txt", 
                         header = T, skip = 4, stringsAsFactors = FALSE, 
                         colClasses = "character", na.strings = "") %>%
   clean_names()
 
+names(forwards)
+names(backwards)
+
 habitat_with_schema <- bind_rows(forwards, backwards) %>%
-  dplyr::rename(caab_code = code) %>%
   dplyr::mutate(caab_code = as.numeric(caab_code)) %>%
-  dplyr::mutate(caab_code = case_when(
-    broad %in% c("Unknown", "Open Water") ~ 1,
-    broad %in% "Invertebrate Complex" ~ 2,
-    
-    type %in% "Thalassodendrum sp." ~ 63618905, # fix incorrect caab code
-    type %in% "Ecklonia radiata" ~ 54079009, # fix incorrect caab code
-    
-    caab_code %in% 90300910 ~ 80300910, # fix incorrect caab code
-    
-    .default = caab_code
-  )) %>%
+  dplyr::select(-level_2, -level_3, -level_4, -level_5, -scientific, -qualifiers) %>%
   dplyr::left_join(schema) %>%
-  dplyr::mutate(sample = str_replace_all(filename, c(".JPG"= "", ".jpg" = ""))) 
+  dplyr::rename(sample = opcode) %>%
+  dplyr::mutate(
+    level_1 = dplyr::if_else(caab_code == 2, "Biota", level_1)
+  )
+
+missing_caab_code <- habitat_with_schema %>%
+  dplyr::filter(is.na(level_1)) %>%
+  distinct(caab_code)
+
+missing_caab_code
+
+names(habitat_with_schema)
 
 distinct_hab_types <- habitat_with_schema %>%
-  select(broad, morphology, type, starts_with("level"), family, genus, species, caab_code) %>%
+  select(starts_with("level"), family, genus, species, caab_code) %>%
   distinct()
 
 missing_caab_code <- habitat_with_schema %>%
-  dplyr::filter(is.na(caab_code)) %>% 
-  distinct(broad, morphology, type) # good
+  dplyr::filter(is.na(level_1)) %>%
+  distinct(caab_code, level_2, level_3)
+
+missing_caab_code_raw <- habitat_with_schema %>%
+  dplyr::filter(is.na(caab_code)) %>%
+  distinct(sample, filename)
 
 unique(habitat_with_schema$sample) %>% sort()
 
@@ -98,41 +105,45 @@ write_csv(tidy_habitat %>%
 
 # RELIEF ----
 # read in forwards annotations
-forwards_relief <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_Habitat_grid_forwards_Dot Point Measurements.txt", 
+forwards_relief <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_Forwards_Relief_Dot Point Measurements.txt", 
                               header = T, skip = 4, stringsAsFactors = FALSE, 
                               colClasses = "character", na.strings = "") %>%
   clean_names()%>%
-  dplyr::filter(!filename %in% "IO333.jpg") %>%
   dplyr::mutate(filename = str_replace_all(filename, "take 2", ""))
 
 # read in forwards annotations
-backwards_relief <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_Habitat_grid_backwards_Dot Point Measurements.txt", 
+backwards_relief <- read.delim("data/2023-03_SwC_stereo-BRUVs/2023-03_SwC_stereo-BRUVs_Backwards_Relief_Dot Point Measurements.txt", 
                                header = T, skip = 4, stringsAsFactors = FALSE, 
                                colClasses = "character", na.strings = "") %>%
   clean_names() 
 
+names(forwards_relief)
+names(backwards_relief)
+
+forwards_relief %>%
+  dplyr::select(filename, relief_annotated, level_2, level_3, level_4, level_5, caab_code, scientific) %>%
+  head(10)
+
+unique(forwards_relief$relief_annotated)
+
 relief_with_schema <- bind_rows(forwards_relief, backwards_relief) %>%
-  dplyr::select(filename, relief) %>%
-  dplyr::mutate(sample = str_replace_all(filename, c(".JPG"= "", ".jpg" = ""))) %>%
-  dplyr::filter(!is.na(relief)) %>%
-  dplyr::mutate(level_5 = str_sub(relief, 2, 2)) %>%
-  dplyr::filter(!level_5 %in% "n") %>%
-  dplyr::left_join(catami) 
+  dplyr::mutate(caab_code = as.numeric(caab_code)) %>%
+  dplyr::filter(!is.na(level_5)) %>%
+  dplyr::rename(sample = opcode)
 
 unique(relief_with_schema$level_5)
 
 relief.missing.metadata <- anti_join(relief_with_schema, metadata, by = c("sample")) %>%
   glimpse()
 
-
-
 tidy_relief <- relief_with_schema %>%
-  dplyr::mutate(sample = str_trim(sample))%>%
+  dplyr::mutate(sample = str_trim(sample)) %>%
   dplyr::mutate(number = 1) %>%                                     
   dplyr::mutate(campaignid = "2023-03_SwC_stereo-BRUVs") %>%
-  dplyr::select(campaignid, sample, number, starts_with("level"), family, genus, species, caab_code) %>%
+  dplyr::mutate(level_1 = "Physical") %>%
+  dplyr::select(campaignid, sample, number, starts_with("level"), caab_code) %>%
   dplyr::filter(!level_2 %in% c("","Unscorable", NA)) %>%  
-  group_by(campaignid, sample, across(starts_with("level")), family, genus, species, caab_code) %>%
+  group_by(campaignid, sample, across(starts_with("level")), caab_code) %>%
   dplyr::tally(number, name = "count") %>%
   ungroup() %>%                                                     
   dplyr::select(campaignid, sample, level_1, everything()) %>%
